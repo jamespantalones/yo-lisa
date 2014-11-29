5841f9e49ab8aba202a914706d216df786ab8b56
James Pants <jamespants@gmail.com>
2014-11-29 11:13:22 +0100

    more models

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,9 +10,14 @@
  */
 angular.module('yoLisaApp').controller('MainCtrl', ['$scope', '$sce', function ($scope, $sce) {
 
-	$scope.articles = {};
-	$scope.activities = {};
+
+	$scope.articles = [];
+	$scope.activities = [];
+	$scope.navItems = [];
+	$scope.globalData = {};
 	$scope.ready = false;
+
+
 
 	Tabletop.init({
 		key: '1sNdUNoHAZTbueB3AhM-wdinCnL-nnI4OocCJn6hecTM',
@@ -22,7 +27,10 @@ angular.module('yoLisaApp').controller('MainCtrl', ['$scope', '$sce', function (
 			$scope.$apply(function(){
 				$scope.articles = data.Features.elements;
 				$scope.activities = data.Activities.elements;
-				console.log($scope.activities);
+				$scope.navItems = data.Nav.elements;
+				$scope.globalData = data.GlobalInfo.elements[0];
+
+
 				$scope.ready = true;
 			});
 			
